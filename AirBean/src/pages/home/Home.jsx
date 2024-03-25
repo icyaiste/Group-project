82701f0a92f516a383da55ae23c7f93569dede10
Aiste <icy.aiste@gmail.com>
2024-03-25 13:18:23 +0100

    added some navigation css

--- AirBean/src/pages/home/Home.jsx
+++ AirBean/src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './home.css';
+import './style/home.css';
 
 function Home() {
     const navigate = useNavigate();
--- AirBean/src/pages/navigation/Navigation.jsx
+++ AirBean/src/pages/navigation/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-//import './Navigation.css';
+import './styling/navigation.css';
 
 function Navigation() {
 
@@ -18,16 +18,21 @@ function Navigation() {
     navigate("/status");
   }
 
-    return (
-      <main>
-        <nav>
-          <img src="#" alt="closing button" />
-        </nav>
+  return (
+    <main className='content'>
+      <nav className='closingBtn'>
+        <img src="#" alt="closing button" />
+      </nav>
+      <section>
         <h1 onClick={goToMenuPage}>Meny</h1>
+        <div className='line'></div>
         <h1 onClick={goToAboutPage}>Vårt kaffe</h1>
+        <div className='line'></div>
         <h1 onClick={goToStatusPage}>Orderstatus</h1>
-      </main>
-    )
-  }
+        <div className='line'></div>
+      </section>
+    </main>
+  )
+}
 
-  export default Navigation;
+export default Navigation;

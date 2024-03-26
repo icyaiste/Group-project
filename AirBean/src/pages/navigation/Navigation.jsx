345c6ffe1ac38fe98471e1036cf08ad211ac63fa
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2024-03-26 12:37:32 +0100

    conflicts resolved

--- AirBean/src/pages/navigation/Navigation.jsx
+++ AirBean/src/pages/navigation/Navigation.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './styling/navigation.css';
 
 function Navigation() {
 
@@ -17,10 +19,20 @@ function Navigation() {
   }
 
   return (
-    <div>
-      
-    </div>
+    <main className='content'>
+      <nav className='closingBtn'>
+        <img src="#" alt="closing button" />
+      </nav>
+      <section>
+        <h1 onClick={goToMenuPage}>Meny</h1>
+        <div className='line'></div>
+        <h1 onClick={goToAboutPage}>Vårt kaffe</h1>
+        <div className='line'></div>
+        <h1 onClick={goToStatusPage}>Orderstatus</h1>
+        <div className='line'></div>
+      </section>
+    </main>
   )
 }
 
-export default Navigation
+export default Navigation;

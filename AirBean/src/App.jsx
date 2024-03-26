44bb02662dc16cad693e174faaca621531445059
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2024-03-26 09:48:49 +0100

    Branch created

--- AirBean/src/App.jsx
+++ AirBean/src/App.jsx
@@ -1,9 +1,20 @@
 import './App.css'
 
+import router from './router/router';
+import { BrowserRouter, Routes, Route, RouterProvider } from 'react-router-dom'
+
+import About from './pages/about/About';
+import Home from './pages/home/Home';
+import Menu from './pages/menu/Menu';
+import Navigation from './pages/navigation/Navigation';
+import Status from './pages/status/Status';
+import Cart from './pages/cart/Cart';
+
 function App() {
 
   return (
     <main>
+      <RouterProvider router={router} />
     </main>
   )
 }

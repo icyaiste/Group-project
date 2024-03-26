456fe539fdcf07989019fc0e56be32fd12425ffe
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2024-03-26 10:06:56 +0100

    coded added

--- AirBean/src/pages/navigation/Navigation.jsx
+++ AirBean/src/pages/navigation/Navigation.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './styles.css';
 
 function Navigation() {
+
+  const navigate = useNavigate();
+
+  const goToMenuPage = () => {
+    navigate("/menu");
+  }
+
+  const goToAboutPage = () => {
+    navigate("/about");
+  }
+
+  const goToStatusPage = () => {
+    navigate("/status");
+  }
+
   return (
-    <div>
-      
-    </div>
+    <main className='content'>
+      <nav className='closingBtn'>
+        <img src="#" alt="closing button" />
+      </nav>
+      <section className='pages'>
+        <h1 onClick={goToMenuPage}>Meny</h1>
+      <hr />
+        <h1 onClick={goToAboutPage}>Vårt kaffe</h1>
+        <hr />
+        <h1 onClick={goToStatusPage}>Orderstatus</h1>
+    <hr />
+      </section>
+    </main>
   )
 }
 
-export default Navigation
+export default Navigation;
